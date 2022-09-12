27c9c673d10ed27cd9c890870cf14692901f7e70
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-09-12 23:45:06 +0200

    Created the Review class and configured it with Course class

--- hb-04-one-to-many-uni/src/hibernate/demo/entity/Course.java
+++ hb-04-one-to-many-uni/src/hibernate/demo/entity/Course.java
@@ -3,12 +3,17 @@ package hibernate.demo.entity;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "course")
@@ -26,6 +31,10 @@ public class Course {
 						CascadeType.PERSIST, CascadeType.REFRESH})
 	@JoinColumn(name = "instructor_id")
 	private Instructor instructor;
+	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "course_id")
+	private List<Review> reviews;
 	
 	public Course() {
 		
@@ -58,13 +67,26 @@ public class Course {
 	public void setInstructor(Instructor instructor) {
 		this.instructor = instructor;
 	}
+	
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+	
+	public void addReview(Review review) {
+		if(reviews == null) {
+			reviews = new ArrayList<>();
+		}
+		reviews.add(review);
+	}
 
 	@Override
 	public String toString() {
-		return "Course [id=" + id + ", title=" + title + "]";
+		return "Course [id=" + id + ", title=" + title + ", instructor=" + instructor + ", reviews=" + reviews + "]";
 	}
-	
-	
 	
 	
 }
