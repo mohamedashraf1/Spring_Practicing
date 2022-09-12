35cf89aabb8d76fc0711c780dba0fc14328e6754
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-09-12 23:44:17 +0200

    changed the fetch type of courses in Instructor to lazy

--- hb-04-one-to-many-uni/src/hibernate/demo/entity/Instructor.java
+++ hb-04-one-to-many-uni/src/hibernate/demo/entity/Instructor.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -37,7 +38,8 @@ public class Instructor {
 	private InstructorDetail instructorDetail;
 	
 	// refers to "instructor" property in "Course" class
-	@OneToMany(mappedBy = "instructor", 
+	@OneToMany(fetch = FetchType.LAZY
+			,mappedBy = "instructor", 
 			cascade = {CascadeType.DETACH , CascadeType.MERGE, 
 					CascadeType.PERSIST, CascadeType.REFRESH})
 	private List<Course> courses;
