ed0a0729e9eadf4335092c2539d94fe73575c9f1
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-09-27 20:17:22 +0200

    apply pointcut to multiple functions

--- spring-demo-aop-pointcut-declarations/src/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-declarations/src/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -13,9 +14,27 @@ public class MyDemoLoggingAspect {
 //	this will match on any modifier(optional) and any return type 
 //	in aopdemo.dao package in any class on any method
 //	with 0 to many arguments of any type
-	@Before("execution(* aopdemo.dao.*.*(..))")
+	@Pointcut("execution(* aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	
+	// apply point cut declaration on this method
+	@Before("forDaoPackage()")
 	public void beforeAddAccountAdvice() {
 		
 		System.out.println("\n=====>>>>> Executing @Before advice on method");
 	}
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalaytics(){
+		System.out.println("\n===> Performing API Analytics");
+	}
 }
+
+
+
+
+
+
+
+
