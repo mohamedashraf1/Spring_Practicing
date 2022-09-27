a56d3991881f2023945f5f99addfe64f236fdb06
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-09-27 22:45:24 +0200

    adding different orders for each aspect

--- spring-aop-pointcut-order-aspects/src/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-aop-pointcut-order-aspects/src/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,11 +2,12 @@ package aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class MyDemoLoggingAspect {
 	
 	// apply point cut declaration on this method
