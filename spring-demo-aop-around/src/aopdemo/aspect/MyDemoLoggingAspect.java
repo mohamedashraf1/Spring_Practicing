45da6c2908786247defad41482ea673847f46a33
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-10-03 20:45:20 +0200

    applying @Around on a method
    applying @Around on getFrotune method

--- spring-demo-aop-around/src/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-around/src/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -3,9 +3,11 @@ package aopdemo.aspect;
 import java.util.List;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -18,6 +20,33 @@ import aopdemo.Account;
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+	
+	@Around("execution(* aopdemo.service.*.getFrotune(..))")
+	public Object arrounGetFortune(
+			ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+		
+		// print the method we are advising on
+		String method = proceedingJoinPoint.getSignature().toShortString();
+		System.out.println("\n====>>> Executing @Around on method: " + method);
+		
+		// begin time stamp
+		long begin = System.currentTimeMillis();
+		
+		// execute the method
+		Object result = proceedingJoinPoint.proceed();
+		
+		// end time stamp
+		long end = System.currentTimeMillis();
+		
+		// compute duration and display it
+		long duration = end -begin;
+		System.out.println("\n====>>> Duration: "+ duration/1000.0 + " seconds");
+		
+		return result;
+	}
+	
+	
+	
 	
 	@After("execution(* aopdemo.dao.AccountDAO.findAccounts(..))")
 	public void afterFinallyFindAccountsAdvice(JoinPoint theJoinPoint) {
