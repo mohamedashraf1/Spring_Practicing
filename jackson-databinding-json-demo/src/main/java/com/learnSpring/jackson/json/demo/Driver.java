7c9910c00ed10ebbd194df1fb7d26c79ad8b9d51
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-11-01 10:28:09 +0200

    jackson nested objects and arrays

--- jackson-databinding-json-demo/src/main/java/com/learnSpring/jackson/json/demo/Driver.java
+++ jackson-databinding-json-demo/src/main/java/com/learnSpring/jackson/json/demo/Driver.java
@@ -16,11 +16,15 @@ public class Driver {
 			// read JSON file and map to java POJO:
 			// data/sample-lite.json
 			Student theStudent = 
-					mapper.readValue(new File("data/sample-lite.json"), Student.class);
+					mapper.readValue(new File("data/sample-full.json"), Student.class);
 			
 			// print first and last name
 			System.out.println("First name = " + theStudent.getFirstName());
 			System.out.println("Last name = " + theStudent.getLastName());
+			System.out.println("Last name = " + theStudent.getAddress().getCity());
+			for(String temp: theStudent.getLanguages()) {
+				System.out.println(temp);
+			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
