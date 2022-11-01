5185d611b2270377a91b3247291e211a73013f22
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-11-01 17:22:14 +0200

    Creating Student class and a controller to handle its' requests

---
+++ spring-rest-demo/src/main/java/com/learnSpring/springdemo/rest/StudentRestController.java
@@ -0,0 +1,45 @@
+package com.learnSpring.springdemo.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learnSpring.springdemo.entity.Student;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+	
+	private List<Student> students;
+	
+	// define @PostConstruct to load the student data ... only once
+	@PostConstruct
+	public void loadData() {
+		students = new ArrayList<Student>();
+		
+		students.add(new Student("mohamed", "ashraf"));
+		students.add(new Student("ahmed", "omar"));
+		students.add(new Student("kareem", "momen"));
+	}
+	
+	// define endpoint for "/students" - return a list of students
+	@GetMapping("/students")
+	public List<Student> getStudents(){
+	
+		return students;
+	}
+	
+	// define endpoint for "/students" - return a list of students
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		return students.get(studentId);
+	}
+
+}
--- spring-rest-demo/src/main/webapp/index.jsp
+++ spring-rest-demo/src/main/webapp/index.jsp
@@ -7,6 +7,10 @@
 	
 	
 	<a href="${pageContext.request.contextPath}/test/hello">Hello</a>
+	
+	<br>
+	
+	<a href="${pageContext.request.contextPath}/api/students">Students</a>
 
 </body>
 
