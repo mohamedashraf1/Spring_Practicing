3b71c93d63ece01c1c6bd8579956c188ae23765a
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-11-01 10:19:06 +0200

    jackson demo - processing JSON

---
+++ jackson-databinding-json-demo/src/main/java/com/learnSpring/jackson/json/demo/Driver.java
@@ -0,0 +1,31 @@
+package com.learnSpring.jackson.json.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			// create object mapper
+			ObjectMapper mapper = new ObjectMapper();
+			
+			// read JSON file and map to java POJO:
+			// data/sample-lite.json
+			Student theStudent = 
+					mapper.readValue(new File("data/sample-lite.json"), Student.class);
+			
+			// print first and last name
+			System.out.println("First name = " + theStudent.getFirstName());
+			System.out.println("Last name = " + theStudent.getLastName());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
