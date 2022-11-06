c4fd25782bc7d1a727eb3a0115daf2d0f3450b23
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-11-06 19:55:42 +0200

    adding api for getting specific customer

--- spring-crm-rest-demo/src/main/java/com/learnSpring/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/learnSpring/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class CustomerRestController {
 	@GetMapping("/customers")
 	public List<Customer> getCustomer() {
 		return customerService.getCustomers();
+	}
+	
+	// add mapping for GET /customers/customerId
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		return customerService.getCustomer(customerId);
 	}
 }
