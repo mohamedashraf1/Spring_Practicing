f67a027853efcb48ca7bb450bb049682afa2dd2e
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-11-06 21:25:53 +0200

    added DELETE api for deleting a customer

--- spring-crm-rest-demo/src/main/java/com/learnSpring/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/learnSpring/springdemo/rest/CustomerRestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learnSpring.springdemo.entity.Customer;
@@ -64,5 +65,18 @@ public class CustomerRestController {
 		return theCustomer;
 	}
 
+	// add mapping for DELETE /customers - delete customer
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		Customer theCustomer = customerService.getCustomer(customerId);
 
+		if (theCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
+		
+		customerService.deleteCustomer(customerId);
 
+		return "Deleted customer id - " + customerId;
+	}
+
+}
