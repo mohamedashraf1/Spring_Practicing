7631cd87cbaab42bf33c310e868336d0593ab5ea
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-11-06 19:34:24 +0200

    adding the Rest Controller for customers

---
+++ spring-crm-rest-demo/src/main/java/com/learnSpring/springdemo/rest/CustomerRestController.java
@@ -0,0 +1,26 @@
+package com.learnSpring.springdemo.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learnSpring.springdemo.entity.Customer;
+import com.learnSpring.springdemo.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+
+	// autowire the CustomerService
+	@Autowired
+	private CustomerService customerService;
+
+	// add mapping for GET /customers
+	@GetMapping("/customers")
+	public List<Customer> getCustomer() {
+		return customerService.getCustomers();
+	}
+}
